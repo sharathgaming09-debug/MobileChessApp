9689a58c7d13606b1bdfb316c0bbefdebcdcc4c0
sharathgaming09-debug <sharathgaming09@gmail.com>
2026-09-16 15:47:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 
+
+class ChessBoard(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.cols = 8
+
+        pieces = [
+            "♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜",
+            "♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟",
+            "", "", "", "", "", "", "", "",
+            "", "", "", "", "", "", "", "",
+            "", "", "", "", "", "", "", "",
+            "", "", "", "", "", "", "", "",
+            "♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙",
+            "♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"
+        ]
+
+        for piece in pieces:
+            button = Button(
+                text=piece,
+                font_size="32sp"
+            )
+            self.add_widget(button)
+
+
+class ChessApp(App):
+    def build(self):
+        return ChessBoard()
+
+
+if __name__ == "__main__":
+    ChessApp().run()
